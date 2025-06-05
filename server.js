f7782a5a20be074e513b606d89b69e6e7a4f61a9
shubhmane99 <69582236+shubhmane99@users.noreply.github.com>
2025-06-05 17:08:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -224,5 +224,6 @@ app.get("/test-java", async (req, res) => {
 
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(8080, () => console.log("Server running on port... 8080"));
+
 
